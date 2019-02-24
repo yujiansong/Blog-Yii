a9c3ec9f3e8c20bd845eb705cc33404b70e03dc7
bestjiansong <yujiansong6@163.com>
2019-02-24 10:39:46 +0800

    查询构建起queryBuilder

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -24,15 +24,45 @@ use yii\widgets\ActiveForm;
         $psObjs = \common\models\Poststatus::find()->all();
         $allStatus = \yii\helpers\ArrayHelper::map($psObjs, 'id', 'name');
         */
+
+        /*
+        //第二种方法
         $psArray = Yii::$app->db->createCommand('select id, name from poststatus')->queryAll();
         $allStatus = \yii\helpers\ArrayHelper::map($psArray, 'id', 'name');
+        */
+
+        /*
+        //第三种方法
+        $allStatus = (new \yii\db\Query())
+        ->select(['name', 'id'])
+        ->from('poststatus')
+        ->indexBy('id')
+        ->column();
+        */
+
+        /*
+        //第四种方法
+        $allStatus = \common\models\Poststatus::find()
+        ->select(['name', 'id'])
+        ->orderBy('position')
+        ->indexBy('id')
+        ->column();
+        */
     ?>
-    <?= $form->field($model, 'status')->dropDownList($allStatus, ['prompt' => '请选择状态']); ?>
+
+    <?= $form->field($model, 'status')->dropDownList(\common\models\Poststatus::find()
+        ->select(['name', 'id'])
+        ->orderBy('position')
+        ->indexBy('id')
+        ->column(), ['prompt' => '请选择状态']); ?>
     <?= $form->field($model, 'create_time')->textInput() ?>
 
     <?= $form->field($model, 'update_time')->textInput() ?>
 
-    <?= $form->field($model, 'author_id')->textInput() ?>
+    <?= $form->field($model, 'author_id')->dropDownList(\common\models\Adminuser::find()
+        ->select(['nickname', 'id'])
+        ->indexBy('id')
+        ->column(), ['prompt' => '请选择作者']) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? '新增' : '修改', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
